a1c3f937a5165fb7116759174a7869efe75ed485
maiatef066 <maiatef066@gmail.com>
2025-08-24 19:53:34 +0300

    feat(UI) :implemnt clean user-friendly Sidebar UI to navigate for all app feature easly

---
+++ Frontend (client side)/ChatTalkDesktop/src/main/java/com/system/chattalkdesktop/common/sidebarController.java
@@ -0,0 +1,34 @@
+package com.system.chattalkdesktop.common;
+
+
+import com.system.chattalkdesktop.utils.NavigationUtil;
+import com.system.chattalkdesktop.utils.SessionManager;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class sidebarController {
+
+    @FXML
+    public void navigateToProfile( MouseEvent event ) {
+        NavigationUtil.switchScene(event,"/com/system/chattalkdesktop/ProfilePage/Profile.fxml","Profile");
+    }
+    @FXML
+    public void navigateToChat( MouseEvent event ) {
+//        NavigationUtil.switchScene(event,"/com/system/chattalkdesktop/MainChat/ChatApp.fxml","ChatApp❤");
+    }
+    @FXML
+    public void navigateToGroup( MouseEvent event ) {
+//        NavigationUtil.switchScene(event,"/com/system/chattalkdesktop/GroupPage/Group.fxml","Groups❤");
+    }
+    @FXML
+    public void navigateToSearch( MouseEvent event ) {
+//        NavigationUtil.switchScene(event,"/com/system/chattalkdesktop/SearchPage/SearchForUsers.fxml","Find Friends 💌");
+
+    }
+    @FXML
+    public void logout( MouseEvent event ) {
+        SessionManager.getInstance().clearSession();
+//        NavigationUtil.switchScene(event,"/com/system/chattalkdesktop/auth/Login.fxml","login🎶");
+//   NotificationManager.getInstance().c
+    }
+}
