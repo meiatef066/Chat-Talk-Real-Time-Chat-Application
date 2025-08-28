b60371ba6224a1698445268cd830e91c2c249f7b
maiatef066 <maiatef066@gmail.com>
2025-08-28 22:31:05 +0300

    feat: add query methods to support friend request real-time notifications

--- Backend (server side)/ChatTalk_ServerSide/src/main/java/com/system/chattalk_serverside/repository/FriendRequestRepository.java
+++ Backend (server side)/ChatTalk_ServerSide/src/main/java/com/system/chattalk_serverside/repository/FriendRequestRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FriendRequestRepository extends JpaRepository<FriendRequest,Long> {
     List<FriendRequest> findByReceiverAndStatus( User currentUser, FriendRequest.RequestStatus requestStatus );
 
     List<FriendRequest> findBySenderAndStatus( User currentUser, FriendRequest.RequestStatus requestStatus );
+
+    boolean existsBySenderAndReceiverAndStatus( User userSender, User userReceiver, FriendRequest.RequestStatus requestStatus );
+    
+    Optional<FriendRequest> findBySenderAndReceiverAndStatus( User userSender, User userReceiver, FriendRequest.RequestStatus requestStatus );
 }
